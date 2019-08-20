ab78fed6031e6fb3540ad9f7072dee690385fb22
kameda-opst <kameda.haruki@opst.co.jp>
2019-08-21 00:08:35 +0900

    最初のコミット

---
+++ pages/kameda_haruki/first.vue
@@ -0,0 +1,4 @@
+<template src="./first_resources/template.html"></template>
+<script src="./first_resources/script.js"></script>
+//scopedを記述しておくことで、他に影響を与えなくなります。
+<style lang="scss" scoped src="./first_resources/style.scss"></style>
