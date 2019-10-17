431cdca91dc6f151e772ec64ea9027e86641c865
kameda-opst <kameda.haruki@opst.co.jp>
2019-10-17 20:38:05 +0900

    路線を選んで、駅リストを表示できるようにした

--- pages/kameda_haruki/railway/script.js
+++ pages/kameda_haruki/railway/script.js
@@ -1,24 +1,31 @@
 import axios from 'axios'
 
 export default {
-    head: {
-        title: '東京都交通局の駅リスト'
-    },
-    data() {
-        return {
-			stations:[],
-            firstStationName:''
-        }
-    },
-    created() {
-        this.getStationList()
-    },
-    methods: {
-        getStationList: function() {
-            axios.get('/api/kameda_haruki/station_list', {}).then((response) => {
+	head: {
+		title: '東京都交通局の駅リスト'
+	},
+	data() {
+		return {
+			stations: [],
+			railways: [],
+			railway_id:'',
+			testString:''
+		}
+	},
+	created() {
+		this.getRailwayList()
+	},
+	methods: {
+		getRailwayList: function () {
+			axios.get('/api/kameda_haruki/railway_list', {}).then((response) => {
+				this.railways = response.data
+			});
+		},
+		getStationList: function () {
+			let railwayId = this.railway_id;
+			axios.get('/api/kameda_haruki/station_list/'+railwayId).then((response) => {
 				this.stations = response.data
-				this.firstStationName = this.stations[0].station_name_jpn
-            });
-        },
-    },
+			});
+		},
+	},
 }
