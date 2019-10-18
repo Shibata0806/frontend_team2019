21e5c7338332d2480ab0cfcc4bda7d5916337a35
yamazaki.kouki <yamazaki.kouki@opst.co.jp>
2019-10-19 02:13:24 +0900

    css適用

--- pages/yamazaki_kouki/weather/parts/weather.js
+++ pages/yamazaki_kouki/weather/parts/weather.js
@@ -16,7 +16,7 @@ export default {
     created() {
     },
     methods: {
-        //APIから選択された地域の天気情報を取得する
+        //APIから選択された地域の天気情報を取得する(現在）
         getWeatherInfo: function(cityName) {
             axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + ',jp&units=metric&appid=9b6d5ff7b0491b2df21485a4036bf34e').then((response) => {
                 this.city = response.data.name
@@ -24,6 +24,16 @@ export default {
                 this.temp = response.data.main.temp
                 this.humidity = response.data.main.humidity
                 this.speed = response.data.wind.speed
+            });
+        },
+        //APIから選択された地域の天気情報を取得する（予報）
+        getForecastInfo: function(cityName) {
+            axios.get('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + ',jp&units=metric&appid=9b6d5ff7b0491b2df21485a4036bf34e').then((response) => {
+                    this.city = ""
+                    this.weather = ""
+                    this.temp = 0
+                    this.humidity = 0
+                    this.speed = 0
             });
         }
     },
