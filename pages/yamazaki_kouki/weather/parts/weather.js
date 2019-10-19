4a909fc1f82367b59d1b35925409fbbb101d9d77
yamazaki.kouki <yamazaki.kouki@opst.co.jp>
2019-10-19 15:43:29 +0900

    今後の予報表示を実装

--- pages/yamazaki_kouki/weather/parts/weather.js
+++ pages/yamazaki_kouki/weather/parts/weather.js
@@ -1,40 +1,36 @@
 import axios from 'axios'
-
 export default {
-    head: {
-        title: '天気情報表示アプリ'
+  head: {
+    title: '天気情報表示アプリ'
+  },
+  data() {
+    return {
+      city: "",
+      weather: "",
+      temp: 0,
+      humidity: 0,
+      speed: 0,
+      list: []
+    }
+  },
+  created() {},
+  methods: {
+    //APIから選択された地域の天気情報を取得する(現在）
+    getWeatherInfo: function (cityName) {
+      axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + ',jp&units=metric&appid=9b6d5ff7b0491b2df21485a4036bf34e').then((response) => {
+        this.city = response.data.name
+        this.weather = response.data.weather[0].main
+        this.temp = response.data.main.temp
+        this.humidity = response.data.main.humidity
+        this.speed = response.data.wind.speed
+        this.list = []
+      });
     },
-    data() {
-        return {
-            city: "",
-            weather: "",
-            temp: 0,
-            humidity: 0,
-            speed: 0
-        }
-    },
-    created() {
-    },
-    methods: {
-        //APIから選択された地域の天気情報を取得する(現在）
-        getWeatherInfo: function(cityName) {
-            axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + ',jp&units=metric&appid=9b6d5ff7b0491b2df21485a4036bf34e').then((response) => {
-                this.city = response.data.name
-                this.weather = response.data.weather[0].main
-                this.temp = response.data.main.temp
-                this.humidity = response.data.main.humidity
-                this.speed = response.data.wind.speed
-            });
-        },
-        //APIから選択された地域の天気情報を取得する（予報）
-        getForecastInfo: function(cityName) {
-            axios.get('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + ',jp&units=metric&appid=9b6d5ff7b0491b2df21485a4036bf34e').then((response) => {
-                    this.city = ""
-                    this.weather = ""
-                    this.temp = 0
-                    this.humidity = 0
-                    this.speed = 0
-            });
-        }
-    },
+    //APIから選択された地域の天気情報を取得する（予報）
+    getForecastInfo: function () {
+      axios.get('https://api.openweathermap.org/data/2.5/forecast?q=' + this.city + ',jp&units=metric&appid=9b6d5ff7b0491b2df21485a4036bf34e').then((response) => {
+        this.list = response.data.list
+      });
+    }
+  },
 }
