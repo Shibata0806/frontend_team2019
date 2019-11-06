811c4f0e6cd96a0c7952ebe87e6bd68695d5e8f0
kazehiki03 <kazehiki03.dev@gmail.com>
2019-11-06 14:10:25 +0900

    [add] header,footer

---
+++ components/kazehiki03/common/Thefooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">© kazehiki03</footer>
+</template>
+
+<style scoped lang="scss">
+@import "~assets/scss/_valiables.scss";
+.footer {
+  background-color: $color-gray;
+  text-align: center;
+  line-height: 30px;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+}
+</style>
--- pages/kazehiki03/cmslack/index.vue
+++ pages/kazehiki03/cmslack/index.vue
@@ -1,32 +1,42 @@
 <template>
-  <div>Hello World</div>
+    <div>
+    <TheHeader/>
+      <nuxt/>あああああああああああ
+    <TheFooter/>
+  </div>
 </template>
 
 <script>
+import TheHeader from "~/components/kazehiki03/common/TheHeader";
+import TheFooter from "~/components/kazehiki03/common/TheFooter";
 const axios = require("axios");
-const url = "http://localhost:3000/api/kazehiki03/slack"
+const url = "http://localhost:3000/api/kazehiki03/slack";
 
 export default {
-	created() {
-		this.getChanelhistory()
+  components: {
+    TheHeader,
+    TheFooter
   },
-	methods: {
-		getChanelhistory: function() {
-			axios({
-				method: "GET",
-				url: url,
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-				},
-			})
-			.then(response => {
-				console.log("status:", response.status);
-				console.log("body:", response.data);
-			})
-			.catch(err => {
-				console.log("err:", err);
-			});
-		}
-	}
-}
+  created() {
+    this.getChanelhistory();
+  },
+  methods: {
+    getChanelhistory: function() {
+      axios({
+        method: "GET",
+        url: url,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        }
+      })
+        .then(response => {
+          console.log("status:", response.status);
+          console.log("body:", response.data);
+        })
+        .catch(err => {
+          console.log("err:", err);
+        });
+    }
+  }
+};
 </script>
