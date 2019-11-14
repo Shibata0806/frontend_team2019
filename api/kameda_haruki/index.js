c477521c45f19b98d1729101a00732be37170a84
kameda-opst <kameda.haruki@opst.co.jp>
2019-11-14 21:25:30 +0900

    APIから列車位置データを取得するようになった。

--- api/kameda_haruki/index.js
+++ api/kameda_haruki/index.js
@@ -13,6 +13,42 @@ router.get('/railway_list', function (req, res) {
   })
 })
 
+router.get('/railway_detail/:railway_id',function(req,res){
+  let railway_id = req.params.railway_id
+  let db = new sqlite3.Database('db/kameda_haruki/odpt_foropst.sqlite3')
+  db.serialize(function() {
+  db.all(`
+  SELECT
+    railways.same_as as railwayId,
+    railways.name_jpn as railwayName,
+    railways.color as railwayColor,
+    railways.code as railwayCode,
+    railways.train_status_odpt_id as trainStatusOdptId,
+    railways.vehicle_location_available as vehicleLocationAvailable,
+    railways.ascending_order_direction as ascendingOrderDirection,
+    ascendingDirections.name_jpn as ascendingDirectionName,
+    railways.descending_order_direction as descendingOrderDirection,
+    descendingDirections.name_jpn as descendingDirectionName
+  FROM
+    railways
+  INNER JOIN
+    railDirections as ascendingDirections
+  ON
+    railways.ascending_order_direction = ascendingDirections.same_as
+  INNER JOIN
+    railDirections as descendingDirections
+  ON
+    railways.descending_order_direction = descendingDirections.same_as
+  WHERE
+    railways.same_as = '${railway_id}'
+  `,
+	function(err, rows) {
+        res.json(rows)
+    })
+    db.close()
+  })
+})
+
 router.get('/station_list/:railway_id', function (req, res) {
   let railway_id = req.params.railway_id
   let db = new sqlite3.Database('db/kameda_haruki/odpt_foropst.sqlite3')
--- pages/kameda_haruki/railway/script.js
+++ pages/kameda_haruki/railway/script.js
@@ -2,15 +2,18 @@ import axios from 'axios'
 
 export default {
 	head: {
-		title: '東京都交通局の駅リスト'
+		title: '東京都交通局の鉄道路線情報'
 	},
 	data() {
 		return {
 			stations: [],
 			railways: [],
 			railway_id: '',
+			railwayProperty: {},
 			train_status: {},
-			train_status_detail_text: ''
+			train_status_detail_text: '',
+			directionId: '',
+			trainLocations: []
 		}
 	},
 	created() {
@@ -22,9 +25,21 @@ export default {
 				this.railways = response.data
 			});
 		},
+		switchLineSelect: function () {
+			this.directionId = '';
+			this.trainLocations = [];
+			this.getLineInfo();
+		},
 		getLineInfo: function () {
+			this.getRailwayDetail();
 			this.getStationList();
 			this.getTrainStatus();
+		},
+		getRailwayDetail: function () {
+			let railway_id = this.railway_id;
+			axios.get('/api/kameda_haruki/railway_detail/' + railway_id).then((response) => {
+				this.railwayProperty = response.data[0]
+			});
 		},
 		getStationList: function () {
 			let railway_id = this.railway_id;
@@ -42,6 +57,18 @@ export default {
 			axios.get(apiUrl).then((response) => {
 				this.train_status = response.data[0]
 				this.train_status_detail_text = this.train_status['odpt:trainInformationText']['ja']
+			});
+		},
+		getTrainLocation: function () {
+			let odptSettingProperties = require('../../../assets/token/kameda_haruki/odpt_setting.json')
+			let accessKeyPart = '&acl:consumerKey=' + odptSettingProperties['accessKey'];
+			let railway_id = this.railway_id;
+			let apiUrl = 'https://api.odpt.org/api/v4/odpt:Train?';
+			apiUrl += 'odpt:railway=' + railway_id;
+			apiUrl += '&odpt:railDirection=' + this.directionId;
+			apiUrl += accessKeyPart;
+			axios.get(apiUrl).then((response) => {
+				this.trainLocations = response.data
 			});
 		}
 	},
