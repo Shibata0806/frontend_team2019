d8e5137268622fe1572e51c89c1b9770c255c66c
samnsk <mas.shachiku@gmail.com>
2019-12-17 20:49:16 +0900

    [add] タイピングゲームっぽくした

--- pages/nakachi_s_kota/typing/script.js
+++ pages/nakachi_s_kota/typing/script.js
@@ -38,5 +38,27 @@ export default {
             const randomIndex = Math.floor(Math.random()*unsolvedWords.length)
             this.currentWord = unsolvedWords[randomIndex]
         },
+    },
+    computed: {
+        isTypingCorrect() {
+            if(this.typingText == this.currentWord) {
+
+                this.solvedWords.push(this.currentWord)
+                this.currentWordNumber = this.solvedWords.length + 1
+                this.typingText = ''
+
+                if(this.words.length == this.solvedWords.length) {
+
+                    this.solvedWords = []
+                    this.isTrying = false
+                    alert('全問正解！')
+                }
+                this.setRandomText()
+                return true
+            }
+
+            const typingTextLength = this.typingText.length
+            return (this.typingText === this.currentWord.slice(0, typingTextLength))
+        }
     }
 }
