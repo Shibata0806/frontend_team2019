bae5cbdfff58263df6e1338c1c61f2cc18fbcba3
kazehiki03 <kazehiki03.dev@gmail.com>
2020-01-20 19:32:38 +0900

    [fix] ファイルも扱えるようにする

--- api/kazehiki03/index.js
+++ api/kazehiki03/index.js
@@ -2,9 +2,7 @@ import express from "express";
 const router = express.Router();
 const axios = require("axios");
 
-router.get("/slack", function(req, res) {
-
-
+router.get("/slack", function (req, res) {
   // slackのAPIを叩く
   let apiToken = require("../../token/kazehiki03/slackToken.json");
   const token = apiToken["token"];
@@ -24,7 +22,8 @@ router.get("/slack", function(req, res) {
   }).then(response => {
     const approveMark = "white_check_mark";
     const disagreeMark = "no_entry_sign";
-    var approvedMessage = {};
+    var approvedMsgTxt = "";
+    var approvedFiles = [];
     var approvedMessages = [];
 
     console.info("[index.js]");
@@ -32,25 +31,40 @@ router.get("/slack", function(req, res) {
 
     let messages = response.data.messages;
     console.log("取得メッセージ数:", messages.length);
-    console.log("response.data.messages:", messages);
+    // console.log("response.data.messages:", messages);
 
     for (let i = 0; i < messages.length; i++) {
       if (messages[i].type && messages[i].type === "message") {
         if (messages[i].reactions) {
           for (let j = 0; j < messages[i].reactions.length; j++) {
-            //  type=messageでreactionがついていてapproveMarkが1つ以上ついている
             if (
               messages[i].reactions[j].name === approveMark &&
               messages[i].reactions[j].count >= 1
             ) {
-              approvedMessage = { ts: messages[i].ts, text: messages[i].text };
-              console.info(`approvedMessagesにpush`,approvedMessage)
-              approvedMessages.push(approvedMessage);
+              //  type=messageでreactionがついていてapproveMarkが1つ以上ついている場合
+              approvedFiles = [];
+              if (messages[i].files) {
+                // 添付ファイルがある場合
+                if (messages[i].files) {
+                  for (let k = 0; k < messages[i].files.length; k++) {
+                    approvedFiles.push({
+                      fileType: messages[i].files[k].filetype,
+                      fileSize: messages[i].files[k].size,
+                      privateUrl: messages[i].files[k].url_private,
+                      publicUrl: messages[i].files[k].permalink_public
+                    })
+                    console.info(`!!!`, approvedFiles);
+                  }
+                }
+              }
+              approvedMsgTxt = { ts: messages[i].ts, text: messages[i].text };
+              approvedMessages.push(approvedMsgTxt, approvedFiles);
+              console.info("\n")
             }
           }
         }
       } else {
-        console.warn("is not message:", messages[i].text);
+        console.log("is not message:", messages[i].text);
       }
     }
     console.log(approvedMessages);
